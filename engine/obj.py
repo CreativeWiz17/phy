8272a5d3289aae7f93c521493f2304a9db5d07a3
Aaditya Aravind <creativewizard0@gmail.com>
2026-03-17 23:03:25 -0500

    added the physics object class  and defined its variables

---
+++ engine/obj.py
@@ -0,0 +1,32 @@
+from engine.vector3 import Vector3
+
+class PhysicsObject: 
+    def __init__(self, mass = 0, position = None, velocity = None, force = None): 
+        
+        self.mass = mass
+
+        if position is None:
+            self.position = Vector3()
+        else:
+            self.position = position
+
+        if velocity is None:
+            self.velocity = Vector3()
+        else:
+            self.velocity = velocity
+
+        if force is None:
+            self.force = Vector3()
+        else:
+            self.force = force
+
+    def addF (self, force):
+
+        self.force = self.force.add(force)
+
+    def acceleration (self):
+
+        a = self.force.divide(self.mass)
+
+        return a
+
