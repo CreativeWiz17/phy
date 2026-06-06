4e0d4e51e4e63b6619975ff9222de30547b82128
Aaditya Aravind <creativewizard0@gmail.com>
2026-06-05 20:23:49 -0500

    Setting Up Dev Diary and Cleaning up some extra test files

--- engine/physicstest.py
+++ engine/physicstest.py
@@ -7,17 +7,14 @@ print(obj1.position) # Object starts at origin
 print(obj1.velocity) # Object also at rest
 print(obj1.acceleration()) # acceleration method called and again 0
 
-obj1.addF(Vector3(10,10,10)) # addF method called and a force of 10 added on every axis
+obj1.addF(Vector3(2,10,10)) # addF method called and a force of 10 added on every axis
 
 print(obj1.force) #printing out the force
 print(obj1.acceleration()) #printing out the accelration, which is a = f/m
 
-obj1.update(1) # amount of time passed, dt = 1
+obj1.update(15)  
 print(obj1.position) # new position of the object after 1 second
 print(obj1.velocity) # new velocity of the object after 1 second
 
 print(obj1.force) # new force, should go back to 0 obv
 
-obj1.update(1) # another second passes
-print(obj1.position) # new position of the object after 2 second
-print(obj1.velocity) # new velocity of the object after 2 second
--- engine/world.py
+++ engine/world.py
@@ -1,4 +1,1 @@
 from vector3 import Vector3
-
-v = Vector3(1, 2, 3)
-print(v)
